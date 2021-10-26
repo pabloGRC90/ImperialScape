3a45fadbd8179bfdd1d69d09632f2ee36a4b1421
PabloGRC90 <pablosiege@gmail.com>
2021-10-26 11:10:40 +0200

    modificación script_login.js

--- js/script_login.js
+++ js/script_login.js
@@ -16,8 +16,7 @@ function validacion()
 
     const emailError = document.querySelector('#mail + span.error');
     const passError = document.querySelector('#password + span.error');
-
-    
+ 
     email.addEventListener('input', function (event) {
         if (email.validity.valid) {
             emailError.innerHTML = 'OK';
@@ -29,7 +28,6 @@ function validacion()
         }
     });
    
-    
     pass.addEventListener('input', function (event) {
         if (pass.validity.valid) {
             passError.innerHTML = 'OK';
@@ -42,30 +40,23 @@ function validacion()
     });
 
     
+    form.addEventListener('submit', function (event) {
+        // si el campo de correo electrónico es válido, dejamos que el formulario se envíe
+        if (!email.validity.valid) {
+            // Si no es así, mostramos un mensaje de error apropiado
+            showErrorMail();
+            // Luego evitamos que se envíe el formulario cancelando el evento
+            event.preventDefault();
+        }
+        if (!pass.validity.valid) {
+            // Si no es así, mostramos un mensaje de error apropiado
+            showErrorPass();
+            // Luego evitamos que se envíe el formulario cancelando el evento
+            event.preventDefault();
+        }
+    });
 
-    // form.addEventListener('submit', function (event) {
-    //     // si el campo de correo electrónico es válido, dejamos que el formulario se envíe
-    //     if (!email.validity.valid) {
-    //         // Si no es así, mostramos un mensaje de error apropiado
-    //         showError();
-    //         // Luego evitamos que se envíe el formulario cancelando el evento
-    //         event.preventDefault();
-    //     }
-    //     if (!pass.validity.valid) {
-    //         // Si no es así, mostramos un mensaje de error apropiado
-    //         showError();
-    //         // Luego evitamos que se envíe el formulario cancelando el evento
-    //         event.preventDefault();
-    //     }
-    // });
-
-   
-   
-   
-   
-   
-    function showError() {
-       
+    function showErrorMail() {
         if (email.validity.valueMissing) {
             // Si el campo está vacío
             // muestra el mensaje de error siguiente.
@@ -79,10 +70,11 @@ function validacion()
             // muestra el mensaje de error siguiente.
             emailError.textContent = 'Debe tener al menos '+ email.minLength +' caracteres; ha introducido '+ email.value.length +'.';
         }
-
         emailError.className = 'error active';
-        
-        
+    }
+   
+    function showErrorPass() {
+       
         if (pass.validity.valueMissing) {
             // Si el campo está vacío
             // muestra el mensaje de error siguiente.
@@ -92,12 +84,7 @@ function validacion()
             // muestra el mensaje de error siguiente.
             passError.textContent = 'La contraseña debe tener al menos '+ pass.minLength +' caracteres; ha introducido '+ pass.value.length +'.';
         }
-
         passError.className = 'error active';
-
-        
-
-        
     }
 }
 
